80f8e0707b5b62b9c680c4e246ed9dfe21c571fe
almamun528 <ayapths@gmail.com>
2025-04-04 23:26:57 +0600

    add 401 403 error

--- restaurant-client/src/Hooks/useAxiosSecure.jsx
+++ restaurant-client/src/Hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 export const axiosSecure = axios.create({
   baseURL: "http://localhost:3001",
 });
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth(); //logOut function from context
   // request  interceptor to add authorization header for every secure call to the api
   axiosSecure.interceptors.request.use(function (config) {
     const token = localStorage.getItem("access-token");
@@ -16,9 +20,13 @@ const useAxiosSecure = () => {
     function (response) {
       return response;
     },
-    function (error) {
+    async function (error) {
       const status = error?.response?.status;
       console.log("error from axios secure Interceptors", status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
