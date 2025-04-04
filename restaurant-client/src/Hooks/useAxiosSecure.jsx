752fcf1594bdc22f0caec3db6fe9b1b98be0f1e5
almamun528 <ayapths@gmail.com>
2025-04-04 23:02:47 +0600

    add interseptor

--- restaurant-client/src/Hooks/useAxiosSecure.jsx
+++ restaurant-client/src/Hooks/useAxiosSecure.jsx
@@ -1,12 +1,16 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 export const axiosSecure = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "http://localhost:3001",
 });
 const useAxiosSecure = () => {
-    
-   return axiosSecure 
+  // request  interceptor to add authorization header for every secure call to the api
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("access-token");
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+  });
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
