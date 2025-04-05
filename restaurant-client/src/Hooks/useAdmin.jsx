486e231d096b3a65c659783737cf6175677f4d26
almamun528 <ayapths@gmail.com>
2025-04-05 14:54:39 +0600

    addItem for admin page

--- restaurant-client/src/Hooks/useAdmin.jsx
+++ restaurant-client/src/Hooks/useAdmin.jsx
@@ -5,11 +5,10 @@ import useAxiosSecure from "./useAxiosSecure";
 const useAdmin = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: isAdmin, isPending:isAdminLoading } = useQuery({
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
     queryKey: ["isAdmin", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/admin/${user.email}`);
-      console.log(res.data);
       return res.data?.admin;
     },
   });
--- restaurant-client/src/Pages/DashBoard/AddItems/AddItems.jsx
+++ restaurant-client/src/Pages/DashBoard/AddItems/AddItems.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../../Components/SectionTitle";
+import { useForm } from "react-hook-form";
 
 const AddItems = () => {
-    return (
-        <>
-            <h2>Itme</h2>
-        </>
-    );
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <SectionTitle heading="Add an Item" subHeading="what's new?" />
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* Name  */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Recipe Name</legend>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              className="input w-full"
+              placeholder="Type recipe name"
+            />
+          </fieldset>
+          <div className="flex gap-3">
+            {/* select Category  */}
+            <select
+              {...register("category", { required: true })}
+              defaultValue="Pick a Framework"
+              className="select select-info my-2 flex-1"
+            >
+              <option disabled={true}>Select a category</option>
+              <option value="salad">Salad</option>
+              <option value="pizza">Pizza</option>
+              <option value="soup">Soup</option>
+              <option value="dessert">Dessert</option>
+              <option value="drinks"> Drinks</option>
+            </select>
+            {/* Price  */}
+            <fieldset className="fieldset mt-1 flex-1">
+              <input
+                {...register("price", { required: true })}
+                type="number"
+                className="input w-full"
+                placeholder="Price "
+              />
+            </fieldset>
+          </div>
+
+          <textarea
+            {...register("recipe", { required: true })}
+            type="text"
+            className="textarea w-full"
+            placeholder="Details"
+          ></textarea>
+
+          <input
+            {...register("image", { required: true })}
+            type="file"
+            className="file-input my-2"
+          />
+          <br />
+
+          <button className="btn btn-primary mt-2" type="submit">
+            Add Item
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default AddItems;
